a672562249438ec86233d2618d991f7c002e5533
Saki <sbasken@gmail.com>
2023-02-26 11:25:52 -0800

    add state and effct to fetch dog image

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,26 @@
 // create your App component here
+import React, { useEffect, useState } from 'react'
+
+const App = () => {
+    const [ image, setImage ] = useState("")
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(res => res.json())
+        .then(data => setImage(data.message))
+    }, [])
+
+    if (!image) {
+        return (
+            <p>Loading...</p>
+        )
+    }
+  
+    return (
+        <div>
+            <img src={image} alt="A Random Dog" />
+        </div>
+    )
+}
+
+export default App
